c44641a1584b32bf8a3f9511b48426e092801815
chilismaug <charles.ross.oh@gmail.com>
2014-12-10 23:46:54 -0500

    require / added back

--- inc/list.php
+++ inc/list.php
@@ -3,8 +3,8 @@
  * Generate <li> elements for tartans dynamically, based on the HTML
  * files present in the tartans HTML directory.
  */
-require_once(dirname(__FILE__) . '../config.php');
-require_once(dirname(__FILE__) . 'tartan.inc');
+require_once(dirname(__FILE__) . '/../config.php');
+require_once(dirname(__FILE__) . '/tartan.inc');
 
 $dir        = TARTAN_HTML_DIR;
 $tartans    = array();
@@ -51,7 +51,7 @@ ksort($list_items); // Sort array of tartan HTML <li> elements by key (display n
 $current_letter = '';
 foreach($list_items as $display_name => $li) {
   $first_letter = substr($display_name, 0, 1);
-  if ($first_letter != $current_letter) {
+  if ($first_letter != $current_letter) {icon
     // If the first letter of this tartan is different than
     // the first letter of the preceding tartan, create a list
     // divider with the new letter
